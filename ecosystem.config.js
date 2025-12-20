0afd30bf0f808d75c2adcf6f83f79e6590f2463c
Dinhdungweb <dungmaster7@gmail.com>
2025-12-20 13:27:40 +0700

    chore: simplify ecosystem.config.js to avoid conflicts with aaPanel

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,14 +4,10 @@ module.exports = {
             name: 'nerd-society',
             script: 'npm',
             args: 'start',
-            instances: 1,
-            autorestart: true,
-            watch: false,
-            max_memory_restart: '3G',
             env: {
                 NODE_ENV: 'production',
-                PORT: 3000
+                PORT: 3000 // Đổi port nếu cần
             },
         },
     ],
-}
+};
